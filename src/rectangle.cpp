ecd14ef81cf2f7f6c336ce10a0fc234eccae0d88
Anthony Scopatz <scopatz@gmail.com>
2014-01-08 19:52:41 -0600

    example 2

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -40,16 +40,16 @@ void Rectangle::move(int dx, int dy)
 }
 
 // Example 1
-double Rectangle::do_with_area(AreaHandlerStruct x) 
-{ 
-    return x.op(10);
-}
+// double Rectangle::do_with_area(AreaHandlerStruct x) 
+// { 
+//     return x.op(10);
+// }
 
 // Example 2
-// void Rectangle::do_with_area(VoidAreaHandlerStruct x) 
-// { 
-//     x.op(10);
-// }
+void Rectangle::do_with_area(VoidAreaHandlerStruct x) 
+{ 
+     x.op(10);
+}
 
 // Example 3
 // void Rectangle::do_with_area(area_handler callback, void* context) 
--- src/rectangle.h
+++ src/rectangle.h
@@ -4,11 +4,11 @@ namespace shapes {
 
 
     // Example 1.  As per unit tests, works fine...
-    typedef struct AreaHandlerStruct { double (*op)(int); } AreaHandlerStruct;
+    // typedef struct AreaHandlerStruct { double (*op)(int); } AreaHandlerStruct;
 
     // Example 2.  Doesn't like void in callback function, see stacktrace2.txt
     // This fails at generation of the wrapper class, before even the callback
-    // typedef struct VoidAreaHandlerStruct { void (*op)(int); } VoidAreaHandlerStruct;
+    typedef struct VoidAreaHandlerStruct { void (*op)(int); } VoidAreaHandlerStruct;
 
     // Example 3.  How can I expose a typedef??
     // Fails to generate the callback, see stacktrace2.txt
@@ -29,10 +29,10 @@ namespace shapes {
         void move(int dx, int dy);
 
         // Example 1. 
-        double do_with_area(AreaHandlerStruct x);
+        // double do_with_area(AreaHandlerStruct x);
 
         // Example 2.
-        // void do_with_area(VoidAreaHandlerStruct x);
+        void do_with_area(VoidAreaHandlerStruct x);
 
         // Example 3
         // void do_with_area(area_handler, void*);
